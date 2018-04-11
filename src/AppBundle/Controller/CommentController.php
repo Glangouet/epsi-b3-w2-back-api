16f86fb44acdce49e8dc7b5de438fbc74bf0bfa7
Guillaume <guillaume@dialo-home.com>
2018-04-11 21:17:15 +0200

    update api

---
+++ src/AppBundle/Controller/CommentController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/04/18
+ * Time: 20:32
+ */
+
+namespace AppBundle\Controller;
+
+
+class CommentController
+{
+
+}
---
+++ src/AppBundle/Controller/PrivilegeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/04/18
+ * Time: 20:31
+ */
+
+namespace AppBundle\Controller;
+
+
+class PrivilegeController
+{
+
+}
